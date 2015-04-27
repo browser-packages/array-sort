96e37f43f7621686e32f47fcec673aaad030e6d9
Gabriel Conceição <gabriel_7340@hotmail.com>
2015-04-27 19:24:35 +0100

    Update and rename bp-array-sort.js to index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+function sort ( arr, fn ){
+    if( arr.length <= 1 ) return arr;
+    if( !fn ) var fn = function(){
+        return Math.floor(( Math.random() * arr.length ) + 0 );
+    }
+
+    for( var i = 0; i < ( arr.length - 1 ); i++ ){
+        var res = parseInt( fn( arr[ i ], arr[ i + 1 ] ) ),
+            arrEntry = undefined;
+
+        if( res && ( i + ( res ) ) >= 0 && ( i + ( res ) ) < arr.length && fn( arr[ i ], arr[ i + ( res ) ] ) ){
+            arrEntry = arr[ i + ( res ) ];
+            arr[ i + ( res ) ] = arr[ i ];
+            arr[ i ] = arrEntry;
+        }
+    }
+    return arr;
+};
+
+module.exports = sort;
+
+sort.prototify = function () {
+    if ( Array.prototype.sort === sort ) {
+        return false;
+    }
+
+    Array.prototype.nativeSort = Array.prototype.sort;
+    Array.prototype.sort = sort;
+
+    return true;
+};
